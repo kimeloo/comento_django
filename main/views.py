5eb1a048279e6fc59f2813d381ccd4398d3e4e27
kimeloo <iroo200121@gmail.com>
2024-08-15 23:30:30 +0900

    ADD : content_detail page
    /main/<int>/ 경로에 상세 페이지 연결

--- main/views.py
+++ main/views.py
@@ -6,3 +6,8 @@ def index(request):
     content_list = MainContent.objects.order_by('-pub_date')
     context = {'content_list': content_list}
     return render(request, 'main/content_list.html', context)
+
+def detail(request, content_id):
+    content_list = MainContent.objects.get(id=content_id)
+    context = {'content_list':content_list}
+    return render(request, 'main/content_detail.html', context)

e7361fe05f32af2c6c00cc526a7d2b40c0156a62
kimeloo <iroo200121@gmail.com>
2024-08-15 23:46:27 +0900

    Add : Login 페이지
    Login 이후 '/'로 리다이렉트
    
    /account/login/

--- my_project/urls.py
+++ my_project/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('main/', include('main.urls')),
     path('', include('pages.urls')),
+    path('accounts/', include('accounts.urls')),
 ]

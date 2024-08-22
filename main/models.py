476e96490a46b7a3bfc797a57a80ee5eeff4ea4c
kimeloo <iroo200121@gmail.com>
2024-08-22 23:32:17 +0900

    Fix : Model.Comment field name
    create_Date -> create_date

--- main/models.py
+++ main/models.py
@@ -6,9 +6,12 @@ class MainContent(models.Model):
     content = models.TextField()
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.title
+
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content_list = models.ForeignKey(MainContent, on_delete=models.CASCADE)
     content = models.TextField()
-    create_Date = models.DateTimeField(auto_now_add=True)
+    create_date = models.DateTimeField(auto_now_add=True)
     modify_date = models.DateTimeField(auto_now=True)
